622b16b1d955e4a6eef07f515e86d7fc316b1b4d
Yogesh070 <46161212+Yogesh070@users.noreply.github.com>
2019-08-07 22:10:15 +0545

    small changes

--- Lab 2.0/q6.cpp
+++ Lab 2.0/q6.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 //fncn overloading
-void multiply(int a, int b)
-{
-	cout<<"multiply a*b = "<< a*b<<endl;
-}
+//void multiply(int a, int b)
+//{
+//	cout<<"multiply a*b = "<< a*b<<endl;
+//}
 void multiply(int a, int b,int c)
 {
 	cout<<"multiply a*b*c = "<< a*b*c<<endl;
 }
-void multiply()
-{
-	int a,b,c;
-	cout<<" multiply = "<< a*b*c;
-}
+//void multiply()
+//{
+//	int a,b,c;
+//	cout<<" multiply = "<< a*b*c;
+//}
 main()
 {
 	int a,b,c;
--- Lab 3.1/q6.cpp
+++ Lab 3.1/q6.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 class item{
 	static int count;
 	public:
 	void getdata()
 	{
-		count=count+1;
-		cout<<"the no of times called="<<count;
+		count++;
+		cout<<"the no of time called="<<count<<endl;
 	}
 };
+int item:: count;
 main()
 {
-	item obj;
-	item obj1;
-	item obj2;
+	item obj,obj1,obj2;
 	obj.getdata();
 	obj1.getdata();
 	obj2.getdata();
