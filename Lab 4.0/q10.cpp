6a12dfb0cae5f153443330b966e7d7026183c6a7
Yogesh070 <46161212+Yogesh070@users.noreply.github.com>
2019-08-22 02:07:08 +0545

    added the last question soln

---
+++ Lab 4.0/q10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class mountain{
+	int height;
+	string name,location;
+	public:
+		mountain(string x,int y ,string z){
+			name=x;
+			height=y;
+			location=z;
+		}
+		mountain(string x,double y ,string z){
+			name=x;
+			height=y;
+			location=z;
+		}
+		void displayInfo()
+		{
+			cout<<"the information of the heighest mountain is"<<endl;
+			cout<<"Name:"<<name<<endl;
+			cout<<"height:"<<height<<endl;
+			cout<<"location:"<<location<<endl;		
+		}
+		void CmpHeight(mountain obj2)
+		{
+			if (height>obj2.height)
+			displayInfo();
+			else
+			obj2.displayInfo();
+		}
+};
+main()
+{
+	mountain obj1("Everest",8848,"Sagarmathas");
+	mountain obj2("Machapuchhre",8548,"Nepal");
+	obj1.CmpHeight(obj2);
+}
